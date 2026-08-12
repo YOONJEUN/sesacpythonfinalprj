b0f9d60b25c010a0ac7c0c6f08a9c83cf57de42d
YOONJEUN <001.t3st@gmail.com>
2026-08-12 16:25:48 +0900

    폰트

--- app.py
+++ app.py
@@ -36,7 +36,6 @@ from src.data.save_file import save_processed_data
 
 st.set_page_config(page_title="서울 공공자전거 수요 불균형 분석", layout="wide", page_icon="🚲")
 sns.set_theme(style="whitegrid", palette="deep")
-plt.rcParams["font.family"] = "Malgun Gothic"
 plt.rcParams["axes.unicode_minus"] = False
 
 
